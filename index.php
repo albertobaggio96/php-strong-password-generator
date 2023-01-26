dec3e8549fc9231bc91d6acb7c416de7394d43a0
albertobaggio96 <albertobaggio96@gmail.com>
2023-01-26 15:29:52 +0100

    bassed of password generator

--- index.php
+++ index.php
@@ -1,5 +1,18 @@
 <?php
+  $letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  $numbers = "0123456789";
+  $symbols = "|\!£$%&/()=?^*§#@°";
 
+  function getKindOfCharacters($arrayoOne = "", $arrayTwo = "", $arrayThree = ""){
+    return $arrayoOne.$arrayTwo.$arrayThree;
+  }
+  function getRandomLetter($array, $number){
+    $randomString = "";
+    for ($i = 0; $i < $number; $i++){
+      $randomString .= $array[rand(0, strlen($array) - 1)];
+    }
+    return $randomString;
+  }
 ?>
 
 <!DOCTYPE html>
@@ -13,6 +26,18 @@
    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 </head>
 <body>
-  
+  <form action="./index.php" method="GET">
+    <input type="number" name="numberLenght" id="number-lenght">
+    <button type="submit">Invia</button>
+  </form>
+  <?php
+    if(isset($_GET["numberLenght"])){
+      $characters = getKindOfCharacters($letters, $numbers, $symbols);
+      $passwordGenerated = getRandomLetter($characters, $_GET["numberLenght"]);
+      echo "<div> {$passwordGenerated} </div>";
+    }
+  ?>
+
+  </div>
 </body>
 </html>
